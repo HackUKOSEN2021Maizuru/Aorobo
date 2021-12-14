af584937560a3cc26a01d1a7229b227670962a50
koouu <38750134+koouu@users.noreply.github.com>
2021-12-14 23:38:14 +0900

    title

--- app/src/main/java/com/example/aorobo/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/example/aorobo/ui/gallery/GalleryFragment.java
@@ -4,13 +4,16 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.Navigation;
 
 import com.example.aorobo.R;
 import com.example.aorobo.databinding.FragmentGalleryBinding;
@@ -28,19 +31,31 @@ public class GalleryFragment extends Fragment {
         binding = FragmentGalleryBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textGallery;
-        galleryViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+
+        return root;
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        ((AppCompatActivity)getActivity()).getSupportActionBar().hide();
+    }
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        //((AppCompatActivity)getActivity()).getSupportActionBar().hide();
+        ImageButton imageButton = view.findViewById(R.id.app_start_button);
+        imageButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onClick(View v) {
+                Navigation.findNavController(v).navigate(R.id.action_title_to_home);
             }
         });
-        return root;
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
+        ((AppCompatActivity)getActivity()).getSupportActionBar().show();
         binding = null;
+
     }
 }
