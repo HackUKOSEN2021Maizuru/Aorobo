005c602809493b735a5ec7d10d145ce20d6040c9
nakaji-nandaina <e8571@g.maizuru-ct.ac.jp>
2021-12-17 01:43:07 +0900

    グラフのフラグメント

---
+++ app/src/main/java/com/example/aorobo/ui/chart/ChartFragment.java
@@ -0,0 +1,29 @@
+package com.example.aorobo.ui.chart;
+
+import androidx.fragment.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.aorobo.R;
+import com.example.aorobo.databinding.FragmentBluetoothBinding;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.HashSet;
+
+import android.content.Intent;
+import android.content.Context;
+
+public class ChartFragment extends Fragment {
+
+}
